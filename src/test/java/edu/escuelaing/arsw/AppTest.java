722173142f67150e656af2ce65390fff9aeea082
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-06-02 14:53:11 -0500

    Fixing test assert equals result to final documments

--- src/test/java/edu/escuelaing/arsw/AppTest.java
+++ src/test/java/edu/escuelaing/arsw/AppTest.java
@@ -29,7 +29,7 @@ public class AppTest
     public void testLoc1(){
         try {
             LocClass loc = new LocClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\App.java");
-            Assert.assertEquals(loc.getCont(),56);
+            Assert.assertEquals(loc.getCont(),48);
         } catch (IOException e){
             Assert.fail();
         }
@@ -42,7 +42,7 @@ public class AppTest
     public void testLoc2(){
         try {
             LocClass loc = new LocClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\LocClass.java");
-            Assert.assertEquals(loc.getCont(),69);
+            Assert.assertEquals(loc.getCont(),70);
         } catch (IOException e){
             Assert.fail();
         }
@@ -55,7 +55,7 @@ public class AppTest
     public void testLoc3(){
         try {
             LocClass loc = new LocClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\PhyClass.java");
-            Assert.assertEquals(loc.getCont(),24);
+            Assert.assertEquals(loc.getCont(),25);
         } catch (IOException e){
             Assert.fail();
         }
@@ -68,7 +68,7 @@ public class AppTest
     public void testPhy4(){
         try {
             PhyClass phy = new PhyClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\App.java");
-            Assert.assertEquals(phy.getCont(),new Long(72));
+            Assert.assertEquals(phy.getCont(),new Long(68));
         } catch (IOException e){
             Assert.fail();
         }
@@ -81,7 +81,7 @@ public class AppTest
     public void testPhy5(){
         try {
             PhyClass phy = new PhyClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\LocClass.java");
-            Assert.assertEquals(phy.getCont(),new Long(91));
+            Assert.assertEquals(phy.getCont(),new Long(110));
         } catch (IOException e){
             Assert.fail();
         }
@@ -91,10 +91,10 @@ public class AppTest
      * Test loc 6.
      */
     @Test
-    public void testLoc6(){
+    public void testPhyc6(){
         try {
             PhyClass phy = new PhyClass("C:\\Users\\Acer\\Desktop\\escuela\\inter\\loc-count\\Loc-counting\\src\\main\\java\\edu\\escuelaing\\arsw\\PhyClass.java");
-            Assert.assertEquals(phy.getCont(),new Long(32));
+            Assert.assertEquals(phy.getCont(),new Long(47));
         } catch (IOException e){
             Assert.fail();
         }
